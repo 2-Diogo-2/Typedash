d841726b37091bff39c628e8f1bcf269d6f391ac
Diogo <161363747+2-Diogo-2@users.noreply.github.com>
2024-10-21 13:27:33 -0300

    Delete Projeto directory

--- Projeto/core/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import ResultadosTeste, Dicas, HistoricoTeste  # Certifique-se de importar o modelo HistoricoTeste
-
-admin.site.register(ResultadosTeste)
-admin.site.register(Dicas)
-admin.site.register(HistoricoTeste)  # Adicione esta linha
