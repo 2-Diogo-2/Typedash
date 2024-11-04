0e93441defebd76c411bf3a6659cbc6a9ac9d800
Diogo <161363747+2-Diogo-2@users.noreply.github.com>
2024-11-04 13:12:50 -0300

    Delete core directory

--- core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import HistoricoTeste
-
-@admin.register(HistoricoTeste)
-class HistoricoTestesAdmin(admin.ModelAdmin):
-    list_display = ['usuario', 'erros', 'tempo_total', 'data']  # Altere para 'erros' e 'data'
-    search_fields = ['usuario__username', 'data']  # Ajuste para 'data' em vez de 'data_teste'
-    list_filter = ['usuario', 'data']  # Corrija o filtro para 'data'
